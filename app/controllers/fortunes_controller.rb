e8b3bba5a0c38e2c2636d0426390ccea6c2ad939
Alex Jarvis <alxjrvs@gmail.com>
2016-09-22 09:41:14 -0400

    Beep

---
+++ app/controllers/fortunes_controller.rb
@@ -0,0 +1,30 @@
+class FortunesController < ApplicationController
+
+  def create
+    fortune = Fortune.new(fortune_params)
+    if fortune.save
+      render status: 200
+    else
+      render status: 400
+    end
+  end
+
+  def show
+    json = { 
+      fortune: {
+        body: Fortune.all.sample.body
+      }
+    }.to_json
+    render json: json
+  end
+
+  def index
+    #Ya Don't have to mess with me
+  end
+
+  private
+
+  def fortune_params
+    params.require(:fortune).permit(:body)
+  end
+end
